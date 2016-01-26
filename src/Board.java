56b1cbb68701c1aa6e7f270d16dbd0afec428be5
Taylor Scafe <scafetaylor@gmail.com>
2016-01-26 15:46:10 -0600

    Added setLetter()
    Moved old setLetter() to setSpace()

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Board {
-	Bag objBag = new Bag(); 
+	Bag objBag = new Bag();
 	public Board(){
 		objBag.Shuffle();
 	}
 	
 	private char[][] objBoard = new char[15][15];
-	public boolean setLetter(int space,char input){
+	public boolean setLetter(ArrayList<Tile> tileList){
+		Scanner oScan = new Scanner(System.in);
+		System.out.println(tileList);
+		System.out.print("Choose a Letter or \"-\" to Cancel:  ");
+		char input = oScan.nextLine().charAt(0);
+		if (input == '-'){
+			return false;
+		}
+		for(int i = 0;i<tileList.size();i++){
+			if (input == tileList.get(i).getLetter()){
+				System.out.print("Select position on board:  ");
+				int inputPos = oScan.nextInt();
+				if(setSpace(inputPos, input)){
+					System.out.println("Letter Placed");
+					tileList.remove(i);
+					return true;
+				}
+				else{
+					System.out.println("There is already a letter here!");
+				}
+			}
+		}
+		System.out.println("You do not have this letter");
+		return true;
+	}
+	private boolean setSpace(int space,char input){
 		int x = space/15;
 		int y = space%15;
 		if (objBoard[x][y]<9){
