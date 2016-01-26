851e5b8363bc8d396f9839119e2b8f95fdaa01b2
sfinger <sfinger@sting.graceland.edu>
2016-01-26 15:46:54 -0600

    rdg

--- src/ScrabbleMain.java
+++ src/ScrabbleMain.java
@@ -13,16 +13,18 @@ public class ScrabbleMain {
 		System.out.println("How many players will there be? ");
 		int numOfPlayers = scan.nextInt();
 		
-		for(int i=1; i < numOfPlayers; i++){
+		for(int i=0; i < numOfPlayers; i++){
 			listPlayers.add(new Player(i));
 			objBoard.getTiles(listPlayers.get(i));
 		}
 		
 		boolean notDone = true;
 		while(notDone){
-			for(int i=1; i < numOfPlayers; i++){
-				System.out.println("Player " + i + " tiles:\n" + listPlayers.get(i).getTiles()
-						+ "\n\nBoard:\n" + objBoard.toString());
+			for(int i=0; i < numOfPlayers; i++){
+				System.out.println("Player " + (i+1) + " tiles:\n" + listPlayers.get(i).getTiles()
+						+ "\n\nBoard:\n" + objBoard.toString() + "\n\n");
+				while(objBoard.setLetter(listPlayers)
+						objBoard.setLetter(listPlayers.get(i));
 				
 				
 			}
